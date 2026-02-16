491a11caadf2de287c683a4f12c97702757598f7
Kumi-isaac <ifrimpong259@gmail.com>
2026-02-16 13:13:28 +0000

    fix

--- routes/product.route.js
+++ routes/product.route.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const Product = reqiure("../models/product.model.js");
+const Product = require("../models/product.model.js");
 const router = express.Router();
 const {
   getProducts,
